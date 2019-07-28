cea965d2afc46d450b762276c316129d262976db
sashimimi <mimikhlam@gmail.com>
2019-07-28 11:25:36 -0500

    Add CommandProcessor

---
+++ YouTubeDL-UI/CommandProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeDL_UI
+{
+    public class CommandProcessor
+    {
+        public static void OpenCommandPrompt(string app, string command)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(app);
+            startInfo.CreateNoWindow = true;
+            startInfo.Arguments = command;
+            Process.Start(startInfo);
+        }
+    }
+}
