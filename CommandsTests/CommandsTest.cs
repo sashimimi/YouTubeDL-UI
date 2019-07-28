cb6383cce4e70a3d12183e0615f166dcee925dd8
sashimimi <mimikhlam@gmail.com>
2019-07-28 11:26:12 -0500

    Add command prompt unit tests

---
+++ CommandsTests/CommandsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YouTubeDL_UI;
+
+namespace CommandsTests
+{
+    [TestClass]
+    public class CommandsTest
+    {
+        [TestMethod]
+        public void OpenCommandPromptTest()
+        {
+            string app = "cmd.exe";
+            string command = "/C help"; // a test command
+
+            CommandProcessor.OpenCommandPrompt(app, command);
+        }
+    }
+}
---
+++ CommandsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CommandsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CommandsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2bdb1958-f6b8-4e51-b1e1-57ab56be7ac3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
