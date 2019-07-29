bc09f55aa2337f4495314c83dfd331cb151add0d
sashimimi <mimikhlam@gmail.com>
2019-07-29 12:36:48 -0500

    Add StringHandler class

---
+++ CommandsTests/StringHandlerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YouTubeDL_UI;
+
+namespace CommandsTests
+{
+    [TestClass]
+    public class StringHandlerTest
+    {
+        [TestMethod]
+        public void BuildStringTest()
+        {
+            List<string> strings = new List<string> {"This", "is", "a", "String Builder", "test", "."};
+
+            string str = StringHandler.BuildString(strings);
+
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ YouTubeDL-UI/StringHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeDL_UI
+{
+    public class StringHandler
+    {
+        public static String BuildString(List<string> commands)
+        {
+            string cmd = "";
+            foreach (string command in commands)
+            {
+                cmd += " " + command + " ";
+            }
+            return cmd;
+        }
+    }
+}
